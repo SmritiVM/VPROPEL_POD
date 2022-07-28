86d4dc1d0652e8f8e933368e7780227e8070b937
Smriti Vipin Madangarli <90612640+SmritiVM@users.noreply.github.com>
2022-07-28 21:45:04 +0530

    Update Form Number from tiles.py

--- Form Number from tiles.py
+++ Form Number from tiles.py
@@ -13,10 +13,3 @@ for i in range(len(seq)-q+1):
 
 for i in pos:
     print(i)
-
-##Uttam's code
-t,n = input(),input()
-nl = len(n)
-for i in range(len(t)-nl+1):
-    if sorted(t[i:i+nl])==sorted(n):
-        print(i+1)
