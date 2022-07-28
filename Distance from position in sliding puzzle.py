d7e247337946757c3726e766020c577b79e36d71
Smriti Vipin Madangarli <90612640+SmritiVM@users.noreply.github.com>
2022-07-28 21:42:32 +0530

    Update Distance from position in sliding puzzle.py

--- Distance from position in sliding puzzle.py
+++ Distance from position in sliding puzzle.py
@@ -11,12 +11,3 @@ for i in range(n):
             corr_column = pos%n
             print(matrix[i][j],abs((corr_row-i)*2)+abs(corr_column-j),sep='\t')
 
-##Uttam's code
-n = int(input())
-l = [list(map(int, input().split())) for _ in range(n)]
-s = int(input())
-for i in range(n):
-    for j in range(n):
-        if l[i][j] == -1:continue
-        x = 2*abs(i - (l[i][j] - s)//n) + abs(j - (l[i][j] - s)%n)
-        print(l[i][j],x,sep = '\t') 
